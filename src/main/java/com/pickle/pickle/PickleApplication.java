9a9e09b74dd6541ce826da2a303a24cb35779657
YEJIN <yejin9858@sookmyung.ac.kr>
2023-01-03 21:37:25 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/pickle/pickle/PickleApplication.java
@@ -0,0 +1,13 @@
+package com.pickle.pickle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pickle/pickle/PickleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickle.pickle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
