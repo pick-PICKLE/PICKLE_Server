deded406202b9011490950fd13289a0391bce9a7
starwook <star.wookk@gmail.com>
2023-01-05 23:14:45 +0900

    feat(User.java): 컬럼 설정

--- src/main/java/com/pickle/server/user/domain/User.java
+++ src/main/java/com/pickle/server/user/domain/User.java
@@ -6,15 +6,38 @@ package com.pickle.server.user.domain;
 import com.pickle.server.common.Timestamped;
 import lombok.Getter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
 
 @Getter
 @Entity
 public class User extends Timestamped {
 
     @Id
+    @Column(name ="user_id")
     private String id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String image;
+
+    @Column
+    private String origin; //무슨 로그인?
+
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+//    private List<Reserve> reserves = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+//    private List<DressLike> dressLikes = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+//    private List<StoreLike> storeLikes = new ArrayList<>();
+
+
 
 }
