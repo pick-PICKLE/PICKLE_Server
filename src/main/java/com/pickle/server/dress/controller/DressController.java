95274525b67b251ea19d5af1fe61b7bf1758cb65
YEJIN <yejin9858@sookmyung.ac.kr>
2023-01-07 18:01:43 +0900

    add: dress 관련 파일들 생성

---
+++ src/main/java/com/pickle/server/dress/controller/DressController.java
@@ -0,0 +1,15 @@
+package com.pickle.server.dress.controller;
+
+import com.pickle.server.dress.service.DressService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/dress")
+public class DressController {
+    private final DressService dressService;
+
+}
