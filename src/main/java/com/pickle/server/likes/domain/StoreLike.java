e5e17fa35f062dd275c1a172749c70ba5331bf8b
llprn <yihan0203@naver.com>
2023-01-19 15:27:08 +0900

    feat: 좋아요 조회 관련

---
+++ src/main/java/com/pickle/server/likes/domain/StoreLike.java
@@ -0,0 +1,23 @@
+package com.pickle.server.likes.domain;
+
+import com.pickle.server.store.domain.Store;
+import com.pickle.server.user.domain.User;
+
+import javax.persistence.*;
+
+public class StoreLike {
+    @Id
+    @Column(name ="storelike_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+
+}
---
+++ src/main/java/com/pickle/server/likes/dto/StoreLikeDto.java
@@ -0,0 +1,29 @@
+package com.pickle.server.likes.dto;
+
+import com.pickle.server.likes.domain.Likes;
+import com.pickle.server.store.domain.Store;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class StoreLikeDto {
+    private Long store_id;
+    private String name;
+    private String image;
+    private LocalTime open_time;
+    private LocalTime close_time;
+
+    public StoreLikeDto(Long store_id,String name,String image,LocalTime open_time,LocalTime close_time){
+        this.store_id = store_id;
+        this.name = name;
+        this.image = image;
+        this.open_time = open_time;
+        this.close_time = close_time;
+
+    }
+}
