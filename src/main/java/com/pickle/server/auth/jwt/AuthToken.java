7cb78f4447a872248def41885bad79d70c8bfe64
starwook <star.wookk@gmail.com>
2023-01-24 23:21:01 +0900

    feat: Jwt 토큰,filterting,provider 추가

---
+++ src/main/java/com/pickle/server/auth/jwt/AuthToken.java
@@ -0,0 +1,58 @@
+package com.pickle.server.auth.jwt;
+
+import io.jsonwebtoken.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.security.Key;
+import java.util.Date;
+
+@Slf4j
+@RequiredArgsConstructor
+public class AuthToken {
+    @Getter
+    private final String token;
+    private final Key key;
+
+    private static final String AUTHORITIES_KEY = "role";
+
+    AuthToken(String socialId,  Date expiry, Key key) {
+        this.key = key;
+        this.token = createAuthToken(socialId,  expiry);
+    }
+
+    private String createAuthToken(String socialId,  Date expiry) {
+        return Jwts.builder()  //jwt 토큰 생성
+                .setSubject(socialId)
+//                .claim(AUTHORITIES_KEY, role)
+//                .signWith(key, SignatureAlgorithm.HS256)
+                .setExpiration(expiry)
+                .compact();
+    }
+
+    public boolean validate() {
+        return this.getTokenClaims() != null;
+    }
+
+    public Claims getTokenClaims() {
+        try {
+            return Jwts.parser()
+                    .setSigningKey(key)
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (SecurityException e) {
+            log.info("Invalid JWT signature.");
+        } catch (MalformedJwtException e) {
+            log.info("Invalid JWT token.");
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT token.");
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT token.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT token compact of handler are invalid.");
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/com/pickle/server/auth/jwt/AuthTokenProvider.java
@@ -0,0 +1,69 @@
+package com.pickle.server.auth.jwt;
+
+import com.pickle.server.auth.error.TokenValidFailedException;
+import com.sun.istack.NotNull;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+public class AuthTokenProvider {
+
+    private Long expiry = 30*60*1000L; // 토큰 유효시간 30분
+
+    private final Key key;
+    private static final String AUTHORITIES_KEY = "role";
+
+    public AuthTokenProvider(@Value("${app.auth.tokenSecret}") String secretKey) {  //String 또는 인코딩된 byte개열 비밀키를 가지고 있으면 변형해야함
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    public AuthToken createToken(String id, Long expiry) {  //토큰 생성
+        Date expiryDate = getExpiryDate(expiry);
+        return new AuthToken(id, expiryDate, key);
+    }
+
+    public AuthToken createUserAppToken(String id) { //소셜로그인으로 받아온 Id
+        return createToken(id, expiry);
+    }
+
+    public AuthToken convertAuthToken(String token) {
+        return new AuthToken(token, key);
+    }
+
+    public static Date getExpiryDate(Long expiry) {
+        return new Date(System.currentTimeMillis() + expiry);
+    }
+
+    public Authentication getAuthentication(@NotNull AuthToken authToken) {
+
+        if(authToken.validate()) {
+
+            Claims claims = authToken.getTokenClaims();
+            Collection<? extends GrantedAuthority> authorities =
+                    Arrays.stream(new String[]{claims.get(AUTHORITIES_KEY).toString()})
+                            .map(SimpleGrantedAuthority::new)
+                            .collect(Collectors.toList());
+
+            User principal = new User(claims.getSubject(), "", authorities);
+
+            return new UsernamePasswordAuthenticationToken(principal, authToken, authorities);
+        } else {
+            throw new TokenValidFailedException();
+        }
+    }
+}
