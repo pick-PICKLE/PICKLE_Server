8ee8e3830067300c5cd690c02066958ce32f5d10
starwook <star.wookk@gmail.com>
2023-01-25 00:15:26 +0900

    feat: Jwt 테스트용 이메일 추가

--- src/main/java/com/pickle/server/auth/controller/AuthController.java
+++ src/main/java/com/pickle/server/auth/controller/AuthController.java
@@ -2,9 +2,12 @@ package com.pickle.server.auth.controller;
 
 import com.pickle.server.auth.dto.AuthRequest;
 import com.pickle.server.auth.dto.AuthResponse;
+import com.pickle.server.auth.dto.JoinRequest;
+import com.pickle.server.auth.service.AuthService;
 import com.pickle.server.auth.service.KakaoAuthService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,11 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
     private final KakaoAuthService kakaoAuthService;
+    private final AuthService authService;
 
     @ApiOperation(value = "카카오 로그인", notes = "카카오 엑세스 토큰을 이용하여 사용자 정보 받아 저장하고 앱의 토큰 반환")
     @PostMapping(value ="/kakao")
     public AuthResponse kakaoAuthRequest(@RequestBody AuthRequest authRequest){
         return kakaoAuthService.login(authRequest);
+    }
+
+    @ApiOperation(value ="Jwt 테스트 로그인")
+    @PostMapping(value = "/join")
+    public JSONObject join(@RequestBody JoinRequest joinRequest){
+        return authService.join(joinRequest);
     }
 
 
