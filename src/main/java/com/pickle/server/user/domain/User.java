320f057d0c1dad6c8865ef978b4ee6c4d0795127
roomie-00 <81614420+roomie-00@users.noreply.github.com>
2023-01-26 13:13:54 +0900

    fix: auto_increment 추가

--- src/main/java/com/pickle/server/user/domain/User.java
+++ src/main/java/com/pickle/server/user/domain/User.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 public class User extends Timestamped {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name ="user_id")
     private Long id;
 
