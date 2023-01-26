6fce6f9967d2d745776b8dbd8c866e20b7d41594
starwook <star.wookk@gmail.com>
2023-01-26 21:36:07 +0900

    feat: controller->restcontroller

--- src/main/java/com/pickle/server/common/HomeController.java
+++ src/main/java/com/pickle/server/common/HomeController.java
@@ -3,8 +3,9 @@ package com.pickle.server.common;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
     @GetMapping
