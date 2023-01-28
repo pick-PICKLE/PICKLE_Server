3b1b59e44f19e02ca65262c62c9b4cea4353dfb7
llprn <yihan0203@naver.com>
2023-01-28 16:29:35 +0900

    feat: dress 좋아요 생성,삭제,조회

--- src/main/java/com/pickle/server/dress/controller/DressController.java
+++ src/main/java/com/pickle/server/dress/controller/DressController.java
@@ -1,17 +1,44 @@
 package com.pickle.server.dress.controller;
 
+import com.pickle.server.dress.dto.DressLikeDto;
+import com.pickle.server.dress.dto.UpdateDressLikeDto;
 import com.pickle.server.dress.service.DressService;
+import com.pickle.server.user.domain.User;
+import com.pickle.server.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/dresses")
 public class DressController {
+    @Autowired
     private final DressService dressService;
+    private final UserRepository userRepository;
 
-  //  @PostMapping("/{id}/likes")
+    @GetMapping("/likes/{id}")
+    public Page<DressLikeDto> findDressLikeByUser(@PathVariable("id") Long userId, @PageableDefault(size=10) Pageable pageable) {
+        User user = userRepository.findById(userId).orElseThrow(()->new RuntimeException("해당 id의 유저를 찾을 수 없습니다."));
+        return dressService.findDressLikeByUser(userId,pageable);
+    }
+
+    @PostMapping("/likes")
+    public ResponseEntity<UpdateDressLikeDto> likeDress(@RequestBody UpdateDressLikeDto updatedressLikeDto){
+        dressService.likesDress(updatedressLikeDto);
+        return new ResponseEntity<>(updatedressLikeDto
+                , HttpStatus.valueOf(200));
+    }
+
+    @PostMapping("/likes/delete")
+    public void delLikeDress(@RequestBody UpdateDressLikeDto updatedressLikeDto){
+        dressService.delLikeDress(updatedressLikeDto);
+
+    }
 }
