f49d48aa1044fff1b27a73943565f2a047424885
llprn <yihan0203@naver.com>
2023-01-30 00:33:07 +0900

    add: store 좋아요 관련 파일 추가

---
+++ src/main/java/com/pickle/server/store/domain/StoreLike.java
@@ -0,0 +1,30 @@
+package com.pickle.server.store.domain;
+
+import com.pickle.server.user.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StoreLike {
+    @Id
+    @Column(name ="storeLike_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+}
