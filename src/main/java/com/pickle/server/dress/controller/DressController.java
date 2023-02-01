3c634605d91708d1c119f9112b269dfb5dab0039
llprn <yihan0203@naver.com>
2023-02-01 20:28:32 +0900

    refactor: swagger 어노테이션 적용

--- src/main/java/com/pickle/server/dress/controller/DressController.java
+++ src/main/java/com/pickle/server/dress/controller/DressController.java
@@ -41,12 +41,29 @@ public class DressController {
     public ResponseEntity<DressDetailDto> viewDressDetail(@PathVariable("id") Long dressId, @ApiIgnore @AuthenticationPrincipal User user){
         return new ResponseEntity<>(dressService.findDressDetailInfoByDressId(dressId, user),HttpStatus.OK);
     }
+
+    @ApiOperation(value="의상 좋아요 조회",
+            httpMethod = "GET",
+            response = DressLikeDto.class,
+            notes = "좋아요 조회 API"
+    )
+    @ApiResponses({
+            @ApiResponse(code=200, message= "의상 좋아요 목록 조회 성공")
+    })
     @GetMapping("/likes/{id}")
     public ResponseEntity<List<DressLikeDto>> findDressLikeByUser(@PathVariable("id") Long userId) {
         User user = userRepository.findById(userId).orElseThrow(()->new RuntimeException("해당 id의 유저를 찾을 수 없습니다."));
         return new ResponseEntity<>(dressService.findDressLikeByUser(userId),HttpStatus.OK);
     }
 
+    @ApiOperation(value="의상 좋아요 추가",
+            httpMethod = "POST",
+            response = UpdateDressLikeDto.class,
+            notes = "의상 좋아요 추가 API"
+    )
+    @ApiResponses({
+            @ApiResponse(code=200, message= "의상 좋아요 추가 성공")
+    })
     @PostMapping("/likes")
     public ResponseEntity<UpdateDressLikeDto> likeDress(@RequestBody UpdateDressLikeDto updatedressLikeDto){
         dressService.likesDress(updatedressLikeDto);
@@ -54,6 +71,14 @@ public class DressController {
                 , HttpStatus.OK);
     }
 
+    @ApiOperation(value="의상 좋아요 삭제",
+            httpMethod = "POST",
+            response = UpdateDressLikeDto.class,
+            notes = "의상 좋아요 삭제 API"
+    )
+    @ApiResponses({
+            @ApiResponse(code=200, message= "의상 좋아요 삭제 성공")
+    })
     @PostMapping("/likes/delete")
     public ResponseEntity<UpdateDressLikeDto> delLikeDress(@RequestBody UpdateDressLikeDto updatedressLikeDto){
         dressService.delLikeDress(updatedressLikeDto);
