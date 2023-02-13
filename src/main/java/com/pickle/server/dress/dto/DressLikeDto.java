c6f63d9b4419bd8eb8c7e7fd8efe0157454fecc0
roomie-00 <81614420+roomie-00@users.noreply.github.com>
2023-02-14 02:08:55 +0900

    fix: dressLikeDto 매장명 추가

--- src/main/java/com/pickle/server/dress/dto/DressLikeDto.java
+++ src/main/java/com/pickle/server/dress/dto/DressLikeDto.java
@@ -25,8 +25,8 @@ public class DressLikeDto {
     @JsonProperty("dress_id")
     private Long dressId;
     @ApiModelProperty(example = "의상 이름")
-    @JsonProperty("name")
-    private String name;
+    @JsonProperty("dress_name")
+    private String dressName;
     @ApiModelProperty(example = "의상 가격")
     @JsonProperty("price")
     private String price;
@@ -35,11 +35,16 @@ public class DressLikeDto {
     @JsonProperty("image")
     private String image;
 
+    @ApiModelProperty(example = "스토어 이름")
+    @JsonProperty("store_name")
+    private String storeName;
+
     @QueryProjection
     public DressLikeDto(Dress dress, String imageUrl){
         DecimalFormat priceFormat = new DecimalFormat("###,###");
+        this.storeName = dress.getStore().getName();
         this.dressId = dress.getId();
-        this.name = dress.getName();
+        this.dressName = dress.getName();
         this.price = priceFormat.format(dress.getPrice())+"원";
         this.image = imageUrl;
     }
